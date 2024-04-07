fbe1a272dfc3b563e5139e8edc6703e650623f71
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-04-07 15:11:54 +0200

    feat(frontend): PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,40 @@ import * as path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
+import { VitePWA } from "vite-plugin-pwa";
 import svgrPlugin from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+
+const manifestForPlugIn = {
+    registerType: "prompt",
+    workbox: {
+        globPatterns: [],
+    },
+    includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+    manifest: {
+        name: "DriveQuest",
+        short_name: "DriveQuest",
+        description: "DriveQuest app",
+        theme_color: "#030712",
+        background_color: "#030712",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+        icons: [
+            {
+                src: "logo192.png",
+                sizes: "192x192",
+                type: "image/png",
+            },
+            {
+                src: "logo512.png",
+                sizes: "512x512",
+                type: "image/png",
+            },
+        ],
+    },
+};
 
 export default defineConfig({
     plugins: [
@@ -18,6 +50,9 @@ export default defineConfig({
         }),
         viteTsconfigPaths(),
         svgrPlugin(),
+        //eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        VitePWA(manifestForPlugIn),
     ],
     resolve: {
         alias: {
