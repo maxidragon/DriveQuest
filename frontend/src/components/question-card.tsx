6511ac14f5734e523409afcb5cc31822cb91a349
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-07-08 02:23:44 +0200

    feat(frontend): sort answers in alphabetical order (always)

--- frontend/src/components/question-card.tsx
+++ frontend/src/components/question-card.tsx
@@ -8,12 +8,15 @@ interface QuestionCardProps {
 }
 
 const QuestionCard = ({ question, onAnswerClick }: QuestionCardProps) => {
+    const answersInAlpbabeticalOrder = question.answers.sort((a, b) =>
+        a.text.localeCompare(b.text)
+    );
     return (
         <div className="flex flex-col gap-4 items-center">
             <h1>{question.text}</h1>
             <QuestionAsset question={question} />
             <div className="flex gap-2 items-center justify-center flex-wrap">
-                {question.answers.map((answer, index) => (
+                {answersInAlpbabeticalOrder.map((answer, index) => (
                     <div key={index}>
                         <Button
                             onClick={() => onAnswerClick(answer.id)}
