d7004c1d9152ea27a55c449cbc9ddd423ec52d25
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-07-08 02:23:22 +0200

    chore: add dev docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DbModule } from './db/db.module';
 import { QuestionModule } from './question/question.module';
+import { AppController } from './app.controller';
 
 @Module({
+  controllers: [AppController],
   imports: [DbModule, AuthModule, QuestionModule],
 })
 export class AppModule {}
