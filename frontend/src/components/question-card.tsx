a7de00fa484194080cde2e3a3a7f4da908fcb936
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-07-08 10:41:11 +0200

    Update question-card.tsx

--- frontend/src/components/question-card.tsx
+++ frontend/src/components/question-card.tsx
@@ -8,15 +8,15 @@ interface QuestionCardProps {
 }
 
 const QuestionCard = ({ question, onAnswerClick }: QuestionCardProps) => {
-    const answersInAlpbabeticalOrder = question.answers.sort((a, b) =>
-        a.text.localeCompare(b.text)
-    );
+    const sortedAnswers = question.answers
+        .sort((a, b) => a.text.localeCompare(b.text))
+        .reverse();
     return (
         <div className="flex flex-col gap-4 items-center">
             <h1>{question.text}</h1>
             <QuestionAsset question={question} />
             <div className="flex gap-2 items-center justify-center flex-wrap">
-                {answersInAlpbabeticalOrder.map((answer, index) => (
+                {sortedAnswers.map((answer, index) => (
                     <div key={index}>
                         <Button
                             onClick={() => onAnswerClick(answer.id)}
